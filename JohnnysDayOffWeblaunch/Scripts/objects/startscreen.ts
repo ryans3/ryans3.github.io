e7ff33d93ad9037b5589d83ff242e997a250148a
ryans3 <ryan.s3@hotmail.com>
2015-03-21 00:48:21 -0400

    Debugging

---
+++ JohnnysDayOffWeblaunch/Scripts/objects/startscreen.ts
@@ -0,0 +1,15 @@
+﻿module objects {
+    // OCEAN CLASS
+    export class Startscreen extends createjs.Bitmap {
+        // PUBLIC INSTANCE VARIABLES
+        private _dx: number = -5;
+
+        // CONSTRUCTOR
+        constructor() {
+            super(assetLoader.getResult("startscreen"));
+        }
+
+
+    }
+
+}   
